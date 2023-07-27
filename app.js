15aae2f4e8521d66cd3df9eb15c2068a2c10e0f9
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-07-27 19:39:16 -0400

    file structure simplified

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const { generateVocab } = require("./controllers/openaiController");
 
 // app setup
 const app = express();
@@ -22,6 +21,51 @@ app.get("/", (request, response) => {
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
 });
+
+// OPENAI API
+const { Configuration, OpenAIApi } = require("openai");
+require("dotenv").config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_AI_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+// generateVocab function
+const generateVocab = async (req, res) => {
+  const prompt = `
+          Generate one SAT-level vocabulary term and provide a definition and example. Return HTML like the following:
+          <div id="vocabulary">
+            Ostentatious
+          </div>
+          <div id="definition">
+            Displaying wealth, knowledge, or style in a way that is meant to impress others or attract attention.
+          </div>
+          <div id="example">
+            &ldquo;At the charity gala, Susan arrived wearing a floor-length, ostentatious gown adorned with glittering diamonds, capturing the attention of everyone in the room.&rdquo;
+          </div>
+        `;
+
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+    max_tokens: 1000,
+    temperature: 1.25,
+  });
+
+  // response
+  console.log(response.data.choices[0].message);
+
+  res.status(200).json({
+    response: response.data.choices[0].message,
+  });
+};
 
 // middleware
 app.use(express.json());
