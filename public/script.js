5c868d3a2fab2db301c2e7c6d9395db18869f24e
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-07-26 22:14:03 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -19,3 +19,9 @@ vocabForm.addEventListener("submit", async (e) => {
 
   description.innerHTML = data.description.content;
 });
+
+// copyright year
+const newDate = new Date();
+const fullYear = newDate.getFullYear();
+const cpyr = document.querySelector("#copy-year");
+cpyr.innerHTML = fullYear;
