ec92e55b9f42d7eebdec37008231ca1a3a688fc8
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-07-27 19:18:46 -0400

    index.html converted to pug views

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const path = require("path");
 const { generateVocab } = require("./controllers/openaiController");
 
 // app setup
 const app = express();
 const port = process.env.PORT || "80";
+
+// set up express app to use pug as template engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+// set up public folder path
+app.use(express.static(path.join(__dirname, "public")));
+
+// view controllers
+app.get("/", (request, response) => {
+  response.render("index", { title: "Home" });
+});
 
 //set up server listening
 app.listen(port, () => {
